35a2739efb8bb190abe863e5108cd712f4eeb0c7
Valentin-Technique <valentin.bajou@gmail.com>
2025-07-26 15:11:49 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,27 +6,43 @@ const urlsToCache = [
   '/seance-test/preview.html',
   '/seance-test/lecture.html',
   '/seance-test/historique.html',
+  '/seance-test/manifest.webmanifest',
   '/seance-test/icone-192.png',
   '/seance-test/icone-512.png',
-  '/seance-test/manifest.webmanifest',
-  'https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&display=swap'
+  '/seance-test/style.css',
+  'https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&display=swap',
 ];
 
-self.addEventListener('install', function(event) {
+// Installation → pré-cache les ressources
+self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('[Service Worker] Caching all: app shell and content');
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', function(event) {
+// Activation → nettoyage anciens caches si nécessaire
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+                  .map(name => caches.delete(name))
+      );
+    })
+  );
+});
+
+// Interception des requêtes
+self.addEventListener('fetch', function (event) {
   event.respondWith(
     caches.match(event.request)
-      .then(function(response) {
+      .then(response => {
         return response || fetch(event.request);
+      }).catch(() => {
+        return new Response('⚠️ Vous êtes hors ligne et la ressource n’est pas en cache.', {
+          headers: { 'Content-Type': 'text/plain;charset=utf-8' }
+        });
       })
   );
 });
