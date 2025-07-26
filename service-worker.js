f8c18bf3cd09ef6a9dd765a7c0d4df2ececa1602
Valentin-Technique <valentin.bajou@gmail.com>
2025-07-26 15:06:51 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,32 @@
-// service-worker.js
 const CACHE_NAME = 'seance-cache-v1';
 const urlsToCache = [
   '/',
+  '/seance-test/',
   '/seance-test/index.html',
   '/seance-test/preview.html',
   '/seance-test/lecture.html',
   '/seance-test/historique.html',
-  '/seance-test/manifest.webmanifest',
   '/seance-test/icone-192.png',
   '/seance-test/icone-512.png',
-  'https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&display=swap',
-  // ajoute ici d'autres fichiers statiques si nécessaire
+  '/seance-test/manifest.webmanifest',
+  'https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&display=swap'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('[Service Worker] Caching all: app shell and content');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
   );
 });
