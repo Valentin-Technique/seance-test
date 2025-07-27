305eebd8b97776d68c5c12dda17de2afe17d4f8a
Valentin-Technique <valentin.bajou@gmail.com>
2025-07-27 16:47:04 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // 🆕 Version du cache — incrémente à chaque changement significatif
-const CACHE_VERSION = 'v3';
+const CACHE_VERSION = 'v4';
 const CACHE_NAME = `seance-cache-${CACHE_VERSION}`;
 
 const urlsToCache = [
